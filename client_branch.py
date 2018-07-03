4d89a3a9e0281e501aed4aa9ab07d0b4a6717fb7
SaiChrla <seshasaicharla@gmail.com>
2018-07-03 14:14:00 +0530

    self restart

--- client_branch.py
+++ client_branch.py
@@ -7,15 +7,28 @@ import protocol as p
 from multiprocessing import Process, Lock
 import config
 from filelockio import filelockwrite
+import time
 
-def client_branch(addr, lock):
+
+def client_branch(addr, lock, st):
     """Starts a client to a server and writes the received data in to a
     file"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
-        client_sock.connect(addr)
+        client_sock.settimeout(st)
         frame = bytes()
         trail = bytes()
         frames = []
+        try:
+            client_sock.connect(addr)
+        except KeyboardInterrupt:
+            client_sock.close()
+        except ConnectionRefusedError:
+            client_sock.close()
+            print("ConnectionRefusedError: GCO Server not responding")
+            time.sleep(st)
+            print("Restarting the client to {}".format(addr[0]))
+            client_branch(addr, lock, st)       # Restart the client server
+
         while True:
             try:
                 data = trail + client_sock.recv(p.BUF_SIZ)
@@ -41,15 +54,19 @@ def client_branch(addr, lock):
             except KeyboardInterrupt:
                 client_sock.close()
                 break
-            except:
-                time.sleep(10)
-                client_branch(addr, lock)       # Restart the client server
+            except OSError:
+                client_sock.close()
+                print("TimeOutError: GCO Server not responding")
+                time.sleep(st)
+                print("Restarting the client to {}".format(addr[0]))
+                client_branch(addr, lock, st)       # Restart the client server
+                break    # else the falulty while loop wil not break with 1 key
 
 
-def setup_clientbranches(n, addrs, lock_list):
+def setup_clientbranches(n, addrs, lock_list, st_list):
     """sets up the required no. of client branches """
-    Clients = [Process(target=client_branch, args=(addrs[i],lock_list[i])) for
-            i in range(n)]
+    Clients = [Process(target=client_branch, args=(addrs[i],lock_list[i],
+        st_list[i])) for i in range(n)]
     for client in Clients :
         client.start()
 
@@ -58,4 +75,4 @@ if __name__ == "__main__" :
 
     n, addrs, nparms, st_list = config.get_config()
     lock_list = [Lock() for i in range(n)]
-    setup_clientbranches(n, addrs, lock_list)
+    setup_clientbranches(n, addrs, lock_list, st_list)
