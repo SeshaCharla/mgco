bba5346c3fe0bbf99ef87381406d4dad8bcf07f1
SaiChrla <seshasaicharla@gmail.com>
2018-07-24 11:24:04 +0530

    a basic logging function.

---
+++ basiclogging.py
@@ -0,0 +1,34 @@
+# vim:fileencoding=utf-8
+# author=SaiChrla
+
+
+import logging
+
+
+def log(msg, addr, level=0):
+    """ Logs the message in to a file named after addr with set level"""
+    # log levels
+    # 0 - DEBUG
+    # 1 - INFO
+    # 2 - Warning
+    # 3 - ERROR
+    # 4 - WARNING
+
+    fname = '{}_{}.log'.format(addr[0], str(addr[1]))
+    logging.config(filename=fname, filemode='a', level=logging.DEBUG,
+                    format='%(asctime)s ; %(levelname)s ; %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+    if level==0:
+        logging.debug(msg)
+    elif level==1:
+        logging.info(msg)
+    elif level==2:
+        logging.warning(msg)
+    elif level==3:
+        logging.error(msg)
+    elif level==4:
+        logging.warning(msg)
+    else:
+        logging.debug(msg)
+        logging.debug('The level set is not there, it should be in [0,4]')
+
