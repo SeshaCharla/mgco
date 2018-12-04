58bed122a4dea356ac1312b7bba1130da8c8a83f
SaiChrla <seshasaicharla@gmail.com>
2018-12-04 05:42:07 +0530

    minor errors

--- htr_sts.py
+++ htr_sts.py
@@ -11,13 +11,13 @@ import datetime as dt
 
 n, addr_list, nparms_list, st_list = config.get_config()
 lock = Lock()
-string  = filelockread(addr_list[0], lock)
 frame = p.GCFrame(nparms_list[0])
 hnames = open('htrs.txt').readlines()
 hlist = [i[:-1] for i in hnames]
 while True:
-    frame.update_frame(string)
+    string  = filelockread(addr_list[0], lock)
     if frame.valid(p.Frame(string)):
+        frame.update_frame(string)
         status_str = frame.data[(-14*8):].decode('cp1252')
         htr_sts = [status_str[8*i:8*(i+1)] for i in range(int(len(status_str)/8))]
         htr_sts_dict = dict(zip(hlist, htr_sts))
